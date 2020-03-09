1dabb889f59c86c19af1280def82ecabf5d89071
Jake Kara <jake@jakekara.com>
2020-03-09 11:48:25 -0400

    Updated package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nbmod-jakekara", 
+    name="nbmod", 
     version="0.0.1",
     author="Jake Kara",
     author_email="jake@jakekara.com",
