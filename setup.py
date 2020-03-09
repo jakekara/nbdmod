82bfc52d37a90eff92bca73256232fbb0cb7f850
Jake Kara <jake@jakekara.com>
2020-03-09 11:40:22 -0400

    Initial

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nbmod-jakekara", 
+    version="0.0.1",
+    author="Jake Kara",
+    author_email="jake@jakekara.com",
+    description="Prototype for notebook module system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jakekara/nbmod",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
